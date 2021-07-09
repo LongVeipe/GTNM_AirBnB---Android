7ee0523a7d300178cfcf4122857c74803f567bfc
duykhanhtran2029 <18520075@gm.uit.edu.vn>
2021-07-09 11:30:57 +0700

    Add UI Login, Home, Hosting

---
+++ app/src/main/java/com/example/airbnb/View/HostingRoom/RoomContactActivity.java
@@ -0,0 +1,14 @@
+package com.example.airbnb.View.HostingRoom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoomContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_contact);
+    }
+}
