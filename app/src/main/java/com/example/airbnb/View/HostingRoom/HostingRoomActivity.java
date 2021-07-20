fbb24277418b4c8a7f255426b8ec66e29f8a1996
duykhanhtran2029 <18520075@gm.uit.edu.vn>
2021-07-20 23:34:42 +0700

    fix UI

--- app/src/main/java/com/example/airbnb/View/HostingRoom/HostingRoomActivity.java
+++ app/src/main/java/com/example/airbnb/View/HostingRoom/HostingRoomActivity.java
@@ -1,6 +1,8 @@
 package com.example.airbnb.View.HostingRoom;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -13,15 +15,22 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import com.example.airbnb.R;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HostingRoomActivity extends AppCompatActivity {
-
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.rt_actv)
     AutoCompleteTextView rt_actv;
+    @BindView(R.id.range_actv)
     AutoCompleteTextView range_actv;
+    @BindView(R.id.st_actv)
     AutoCompleteTextView st_actv;
-    String []rt_options = {"Nhà nguyên căn","Chung cư", "Phòng riêng","Nhà hàng", "Homestay","Không gian độc đáo" };
-    String []range_options = {"Toàn bộ","Theo phòng", "Theo khu vực"};
-    String []st_options = {"Riêng tư","Tiệc tùng", "Họp mặt"};
+
+
     ArrayAdapter rt_ad;
     ArrayAdapter range_ad;
     ArrayAdapter st_ad;
@@ -29,23 +38,12 @@ public class HostingRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hosting_room);
-        rt_actv = findViewById(R.id.rt_actv);
-        rt_ad = new ArrayAdapter(this, R.layout.item_option, rt_options);
-        rt_actv.setText(rt_ad.getItem(0).toString(), true);
-        rt_actv.setAdapter(rt_ad);
+        ButterKnife.bind(this);
 
-        range_actv = findViewById(R.id.range_actv);
-        range_ad = new ArrayAdapter(this, R.layout.item_option, range_options);
-        range_actv.setText(range_ad.getItem(0).toString(), true);
-        range_actv.setAdapter(range_ad);
+        initToolbar();
+        initOptions();
 
-        st_actv = findViewById(R.id.st_actv);
-        st_ad = new ArrayAdapter(this, R.layout.item_option, st_options);
-        st_actv.setText(st_ad.getItem(0).toString(), true);
-        st_actv.setAdapter(st_ad);
     }
-
-    @SuppressLint("WrongConstant")
     public void ChangeNumber(View view) {
 
         EditText edt;
@@ -60,7 +58,8 @@ public class HostingRoomActivity extends AppCompatActivity {
         {
             edt = findViewById(R.id.snl);
             number = Integer.parseInt(edt.getText().toString());
-            edt.setText(number - 1 + "");
+            if(number > 0)
+                edt.setText(number - 1 + "");
         }
 
         if(view.getId() == R.id.ste_add)
@@ -73,7 +72,8 @@ public class HostingRoomActivity extends AppCompatActivity {
         {
             edt = findViewById(R.id.ste);
             number = Integer.parseInt(edt.getText().toString());
-            edt.setText(number - 1 + "");
+            if(number > 0)
+                edt.setText(number - 1 + "");
         }
         if(view.getId() == R.id.sgn_add)
         {
@@ -85,7 +85,8 @@ public class HostingRoomActivity extends AppCompatActivity {
         {
             edt = findViewById(R.id.sgn);
             number = Integer.parseInt(edt.getText().toString());
-            edt.setText(number - 1 + "");
+            if(number > 0)
+                edt.setText(number - 1 + "");
         }
         if(view.getId() == R.id.snvs_add)
         {
@@ -97,7 +98,36 @@ public class HostingRoomActivity extends AppCompatActivity {
         {
             edt = findViewById(R.id.snvs);
             number = Integer.parseInt(edt.getText().toString());
-            edt.setText(number - 1 + "");
+            if(number > 0)
+                edt.setText(number - 1 + "");
+        }
+    }
+    private  void initOptions ()
+    {
+        String []rt_options = {"Nhà nguyên căn","Chung cư", "Phòng riêng","Nhà hàng", "Homestay","Không gian độc đáo" };
+        String []range_options = {"Toàn bộ","Theo phòng", "Theo khu vực"};
+        String []st_options = {"Riêng tư","Tiệc tùng", "Họp mặt"};
+
+        rt_ad = new ArrayAdapter(this, R.layout.item_option, rt_options);
+        rt_actv.setText(rt_ad.getItem(0).toString(), true);
+        rt_actv.setAdapter(rt_ad);
+
+        range_ad = new ArrayAdapter(this, R.layout.item_option, range_options);
+        range_actv.setText(range_ad.getItem(0).toString(), true);
+        range_actv.setAdapter(range_ad);
+
+        st_ad = new ArrayAdapter(this, R.layout.item_option, st_options);
+        st_actv.setText(st_ad.getItem(0).toString(), true);
+        st_actv.setAdapter(st_ad);
+    }
+    private void initToolbar() {
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+        {
+            ActionBar actionBar = getSupportActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            setTitle("Thông tin phòng");
         }
     }
 }
